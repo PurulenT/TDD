c46e0a36fd5a246f9e36a9bbde1e529fe7d1efca
Yagmurov <nuri.handsome45@gmail.com>
2023-10-24 17:11:44 +0300

    testing of searchin foldername

--- TDD/Source.cpp
+++ TDD/Source.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 #include <cassert>
+#include <string>
 using namespace std;
 
-short usersChoice(short Choice, short choice)
+short usersChoice(short Choice)
 {
 	cout << "Для заполнения файла нажмите 1\nДля удаления файла нажмите 2\nВвод: ";
-	Choice = choice;
+	cin >> Choice;
 	return Choice;
 }
 
+string Func(string path, string folderName)
+{
+	cout << path + "\\";
+	while (folderName != "con")
+	{
+		cout << "Укажите название файла, для завершения введите con: \n";
+		getline(cin, folderName);
+		if (folderName == "con") break;
+		path = path + '\\';
+		path = path + folderName;
+	}
+	path = path + ".txt";
+	cout << path << endl;
+	return path;
+}
+
+void TestFunc()
+{
+	assert(Func("C:", "testt") == "C:\\testt.txt");
+	cout << "Test OK";
+}
 
 void TestChoice()
 {
-	assert(usersChoice(0, 3) == 3);
-	assert(usersChoice(0, 4) == 4);
+	//assert(usersChoice(0) == Choice);
+	//assert(usersChoice(0) == Choice);
 	cout << "Test OK" << endl;
 }
+
+
 
 
 int main()
 {
 	short Choice;
-	short choice;
+	string path = "0";
+	string folderName;
 	setlocale(LC_ALL, "RUS");
-	TestChoice();
+	//TestChoice();
+	TestFunc();
 	return 0;
 }
