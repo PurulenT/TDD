5e89d04f01be93be34e664699724cd0eafa4332c
Yagmurov <nuri.handsome45@gmail.com>
2023-10-24 16:37:51 +0300

    simplest unit test

--- TDD/Source.cpp
+++ TDD/Source.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <cassert>
 using namespace std;
 
+short usersChoice(short Choice, short choice)
+{
+	cout << "Для заполнения файла нажмите 1\nДля удаления файла нажмите 2\nВвод: ";
+	Choice = choice;
+	return Choice;
+}
+
+
+void TestChoice()
+{
+	assert(usersChoice(0, 3) == 3);
+	assert(usersChoice(0, 4) == 4);
+	cout << "Test OK" << endl;
+}
 
 
 int main()
 {
 	short Choice;
+	short choice;
 	setlocale(LC_ALL, "RUS");
-	cout << "Для заполнения файла нажмите 1\nДля удаления файла нажмите 2\nВвод: ";
-	cin >> Choice;
+	TestChoice();
 	return 0;
 }
