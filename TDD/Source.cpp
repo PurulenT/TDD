989a8bbc5976e6b3baaea80cefb4fdd361fbd946
Yagmurov <nuri.handsome45@gmail.com>
2023-10-24 16:22:01 +0300

    test 3 added first requests

--- TDD/Source.cpp
+++ TDD/Source.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main()
 {
+	short Choice;
+	setlocale(LC_ALL, "RUS");
+	cout << "Для заполнения файла нажмите 1\nДля удаления файла нажмите 2\nВвод: ";
+	cin >> Choice;
 	return 0;
 }
