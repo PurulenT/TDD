cc24de55d6772ab549b7e5e2e0d32a68035be057
Yagmurov <nuri.handsome45@gmail.com>
2023-10-24 17:22:41 +0300

    testing disc choice

--- TDD/Source.cpp
+++ TDD/Source.cpp
@@ -26,6 +26,34 @@ string Func(string path, string folderName)
 	return path;
 }
 
+string Disc(short DC)
+{
+	cout << "Если файл лежит на диске C - нажмите 1\nЕсли файл лежит на диске D - нажмите 2\n" << endl;
+	cout << DC << endl;
+	if (DC == 1)
+	{
+		return "C:";
+	}
+	else if (DC == 2)
+	{
+		return "D:";
+	}
+	else
+	{
+		return "error!";
+	}
+}
+
+void TestDisc()
+{
+	assert(Disc(2) == "D:");
+	assert(Disc(1) == "C:");
+	assert(Disc(3) == "error!");
+	cout << "Test OK";
+
+	
+}
+
 void TestFunc()
 {
 	assert(Func("C:", "testt") == "C:\\testt.txt");
@@ -49,6 +77,7 @@ int main()
 	string folderName;
 	setlocale(LC_ALL, "RUS");
 	//TestChoice();
-	TestFunc();
+	//TestFunc();
+	TestDisc();
 	return 0;
 }
